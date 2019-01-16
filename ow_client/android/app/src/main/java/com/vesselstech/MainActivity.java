a99fcaf353fd63a7dd2592fade5459d5f102849d
Lewis Daly <lewisdaly@me.com>
2019-01-16 20:51:56 +1030

    Finished getting RNN v2 building on Android

--- ow_client/android/app/src/main/java/com/vesselstech/MainActivity.java
+++ ow_client/android/app/src/main/java/com/vesselstech/MainActivity.java
@@ -4,20 +4,21 @@ import android.graphics.Color;
 import android.view.View;
 import android.os.Bundle;
 
-import com.reactnativenavigation.controllers.SplashActivity;
+// import com.reactnativenavigation.controllers.SplashActivity;
+import com.reactnativenavigation.NavigationActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 
-public class MainActivity extends SplashActivity {    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this, false);
-        super.onCreate(savedInstanceState);
-    }
+public class MainActivity extends NavigationActivity {    
+    // @Override
+    // protected void onCreate(Bundle savedInstanceState) {
+    //     SplashScreen.show(this, false);
+    //     super.onCreate(savedInstanceState);
+    // }
 
-    @Override
-    public View createSplashLayout() {
-        View view = new View(this);
-        view.setBackgroundColor(Color.BLUE);
-        return view;
-    }
+    // @Override
+    // public View createSplashLayout() {
+    //     View view = new View(this);
+    //     view.setBackgroundColor(Color.BLUE);
+    //     return view;
+    // }
 }
--- ow_client/android/app/src/main/java/com/vesselstech/MainApplication.java
+++ ow_client/android/app/src/main/java/com/vesselstech/MainApplication.java
@@ -3,6 +3,8 @@ package com.vesselstech;
 import android.app.Application;
 
 import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
 
 import com.facebook.react.ReactApplication;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -35,11 +37,24 @@ import java.util.List;
 
 public class MainApplication extends NavigationApplication {
 
-  @Override
-  public boolean isDebug() {
-      // Make sure you are using BuildConfig from your own application
-      return BuildConfig.DEBUG;
-  }
+
+    //React Native Navigation 2.0
+    @Override
+    protected ReactGateway createReactGateway() {
+        ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+            @Override
+            protected String getJSMainModuleName() {
+                return "index";
+            }
+        };
+        return new ReactGateway(this, isDebug(), host);
+    }
+
+    @Override
+    public boolean isDebug() {
+        // Make sure you are using BuildConfig from your own application
+        return BuildConfig.DEBUG;
+    }
 
   protected List<ReactPackage> getPackages() {
       // Add additional packages you require here
